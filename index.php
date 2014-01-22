5e52472ec99cb50039511f1c847cd381b5dd6468
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-22 08:33:11 +0000

    Test environment is up

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 if ( file_exists( './maint_mode' ) ) {
 	die ( 'This site is on maintenance' );
 }
-exit('Sorry');
+
 /**
  * Front to the WordPress application. This file doesn't do anything, but loads
  * wp-blog-header.php which does and tells WordPress to load the theme.
