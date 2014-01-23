c6031a2eb65b316edc39e91b8bc219c3c36e8382
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-23 16:47:35 +0000

    Verification email changes

--- wp-content/themes/pentaclick/ajax.php
+++ wp-content/themes/pentaclick/ajax.php
@@ -130,7 +130,12 @@ if ($controller == 'registerTeam') {
         );
         
         sendMail($post['email'], 'PentaClick eSports tournament participation', $text);
-        sendMail('pentaclickesports@gmail.com', 'PentaClick eSports tournament participation', 'Team: '.$post['team']);
+        sendMail('pentaclickesports@gmail.com',
+        'Team added. PentaClick eSports.',
+        'Team was deleted.<br />
+        Date: '.date('d/m/Y H:i:s').'<br />
+        Team: <b>'.$post['team'].'</b><br>
+        IP: '.$_SERVER['REMOTE_ADDR']);
     }
 }
 else {
