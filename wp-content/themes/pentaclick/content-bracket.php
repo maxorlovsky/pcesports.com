a11b6e88871cbbf6788139b2952f6da1089071e5
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-24 13:39:25 +0000

    Forgot to add brackets template

---
+++ wp-content/themes/pentaclick/content-bracket.php
@@ -0,0 +1,6 @@
+<div class="holder" id="bracket"></div>
+<article class="bracket-content-wrapper">
+    <div class="content" id="bracket-content">
+        <div id="challonge"></div>
+    </div>
+</article>
