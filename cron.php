71c7c7b4a4267deef57e2f5b6e01d2f569068b6a
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-31 11:44:46 +0000

    getting matches

--- cron.php
+++ cron.php
@@ -5,15 +5,13 @@ if ($_GET['cronjob'] != 'u9a8sdu1209102129dSAD2u1239') {
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
-$answer = runChallongeAPI('tournaments/3ygi2xm0/matches.json', array(), 'state=open');
+$answer = runChallongeAPI('tournaments/pentaclick-lol1/matches.json', array(), 'state=open');
 foreach($answer as $f) {
     //Team ID #1 - $f->match->player1_id;
     //Team ID #2 - $f->match->player2_id;
-    $f->match->player1_id = 12007002; //420gaming
-    $f->match->player2_id = 12007004; //Splendid gaming
     $q = mysql_query('SELECT `id`, `name`, `cpt_player_id` FROM `teams` WHERE `challonge_id` = '.(int)$f->match->player1_id.' OR `challonge_id` = '.(int)$f->match->player2_id);
-    //dump($r);
-    
-    
+    while($r = mysql_fetch_object($q)) {
+        dump($r);
+    }
     //sleep(2);
 }
