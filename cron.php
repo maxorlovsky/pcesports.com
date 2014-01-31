92614bf98580f776ce81f61ef632d50727c1a378
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-31 13:25:18 +0000

    cron script

--- cron.php
+++ cron.php
@@ -5,14 +5,43 @@ if ($_GET['cronjob'] != 'u9a8sdu1209102129dSAD2u1239') {
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
+$text = '
+Team 1: <b>%team1%</b><br />
+Email: <b>%email1%</b><br />
+Contact info: <b>%contact-info1%</b><br />
+<br />
+<br />
+Team 1: <b>%team2%</b><br />
+Email: <b>%email2%</b><br />
+Contact info: <b>%contact-info2%</b>
+<br />
+Please remember that Round 1 games must be played in 3 days starting from now on!<br />
+Use contact info of each other to agree on day and time of your match. Please don\'t forget to add pentaclickesports@gmail.com to CC or add organizers to skype chat.
+<br />
+PentaClick eSports.';
+
 $answer = runChallongeAPI('tournaments/pentaclick-lol1/matches.json', array(), 'state=open');
+$i = 1;
+$players = array();
 foreach($answer as $f) {
     //Team ID #1 - $f->match->player1_id;
     //Team ID #2 - $f->match->player2_id;
-    $q = mysql_query('SELECT `id`, `name`, `email`, `cpt_player_id` FROM `teams` WHERE `challonge_id` = '.(int)$f->match->player1_id.' OR `challonge_id` = '.(int)$f->match->player2_id);
+    $q = mysql_query('SELECT `id`, `name`, `cpt_player_id` FROM `teams` WHERE `challonge_id` = '.(int)$f->match->player1_id.' OR `challonge_id` = '.(int)$f->match->player2_id);
     while($r = mysql_fetch_object($q)) {
-        dump($r);
+        $q2 = mysql_query('SELECT `id`, `name`, `player_id` FROM `players` WHERE `team_id` = '.$r->id.' AND `approved` = 1');
+        $j = 0;
+        while($r2 = mysql_fetch_object($q)) {
+            $players[$i][$j]['id'] = $r2->id;
+            $players[$i][$j]['name'] = $r2->name;
+            $players[$i][$j]['player_id'] = $r2->player_id;
+            ++$j;
+        }
+        ++$i;
     }
+    //sendMail('max.orlovsky.net, pentaclickesports@gmail.com', 'PentaClick tournament - Round 1', $msg);
+    exit();
     echo '<br>---<br>';
+    $emails = '';
+    $i = 1;
     //sleep(2);
 }
