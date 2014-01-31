b4b3ea933e4693da2ac51b43050f06f0b1790865
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-01-31 11:38:03 +0000

    Emails ru

--- wp-content/themes/pentaclick/emails.php
+++ wp-content/themes/pentaclick/emails.php
@@ -5,7 +5,26 @@ if ($_GET['mailjob'] != 'runrun') {
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
-$text = 'Dear team <b>%team%</b>,<br />
+$text = 'Уважаемая команда <b>%team%</b>,<br />
+первый день практически на носу, скоро начнутся первые бои в нашем турнире!<br />
+<br />
+В первую очередь хотелось бы рассказать как будет проходить турнир:<br />
+<br />
+Когда сетка будет доступна, через какое то время вы получите письмо с контактной информацией капитана противников.<br />
+Будет это е-почта или скайп, мы советуем добавлять всегда нашу е-почту (pentaclickesports@gmail.com) в CC (копию), или добавлять нас в общий скайп-чат, где вы будете общатся с капитаном-оппонентом. Скайп имена организаторов - <u>maxorlovsky</u> и <u>facebook:judgement.hasnoname</u><br />
+<br />
+Когда вы будете переписыватся с капитаном-оппонентом, от вас требуется, чтоб вы договорили о дне и времени вашего боя. Всегда учитывайте времяную зону соперников и пожалуйста используйте английский язык для общения, если команда соперник не русско-языная. Так же вам надо будет вместе решить, хотите ли вы, чтоб ваша игра попала на стрим ( перед четверть финалом стрим игр будет доступен только 1 февраля, без комментариев ). Если обе команды соглашаются на стрим, то вы должны нам об этом сообщить и назвать время игры. ( Мы можем задержать время проведения игры по причине, что в тот момент проходит стрим другой игры. Так же, мы скорее всего не сможем стримить полностью все игры ).<br /> 
+<br />
+Битвы в турнире будут засчитаны, если они будут сыграны после 1 фев. 16:00 МСК, когда турнир официально стартует.<br />
+Битвы в каждом раунде должны быть сыграны в течении 3-ёх дней. (исключая раунд 1 и раунд 2, которые должны быть сыграны в течении первых 3 дней).<br />
+<br />
+После того как вы сыграли, вам ничего не нужно делать. Мы получим автоматическую нотификацию и примерно через 60 минут вы увидите ваше продвежение по сетке и получите информацию о вашем следующем оппоненте.<br />
+<br />
+После того как команды достигли четверть финала, турнир возьмёт не большую паузу и будет анонсирован день финальных игр.<br />
+<br />
+PentaClick eSports.';
+
+/*$text = 'Dear team <b>%team%</b>,<br />
 the day almost come upon us for the first days of battles in our tournament!<br />
 <br />
 Here are some outlines about how our tournament will proceed:<br />
@@ -22,18 +41,18 @@ After winning a game you won\'t have to do anything. We will receive results aut
 <br />
 After all teams have reached quarterfinals, the tournament will briefly stop, and we will announce the day of quarterfinal to final games.<br />
 <br />
-PentaClick eSports.';
+PentaClick eSports.';*/
 
 $q = mysql_query('SELECT name, email FROM teams WHERE approved = 1 AND deleted = 0 AND game = "lol" and tournament_id = 1');
 while ($r = mysql_fetch_object($q)) {
-    if (substr($r->email, -2) != 'ru') {
+    if (substr($r->email, -2) == 'ru') {
         $msg = str_replace(
             array('%team%'),
             array($r->name),
             $text
         );
         
-        sendMail($r->email, 'PentaClick LoL tournament #1 - details', $msg);
+        sendMail($r->email, 'PentaClick LoL tournament #1 - детали', $msg);
         
         sleep(2);
     }
