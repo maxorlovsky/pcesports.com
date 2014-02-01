832cfc0ead0f977d7e7de78620ee8e417633ffe1
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-02-01 12:40:28 +0000

    Cron autonotif

--- cron.php
+++ cron.php
@@ -7,29 +7,27 @@ set_time_limit(300);
 require_once $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
 $text = '
-Team 1: <b>%team1%</b><br />
-Email: <b>%email1%</b><br />
-Contact info: <b>%contact-info1%</b><br />
+Team 1: %team1%<br />
 <br />
+Team 2: %team2%<br />
 <br />
-Team 1: <b>%team2%</b><br />
-Email: <b>%email2%</b><br />
-Contact info: <b>%contact-info2%</b>
-<br />
-Please remember that Round 1 games must be played in 3 days starting from now on!<br />
-Use contact info of each other to agree on day and time of your match. Please don\'t forget to add pentaclickesports@gmail.com to CC or add organizers to skype chat.
+Team won: <b>%win%</b>
 <br />
 PentaClick eSports.';
 
 $answer = runChallongeAPI('tournaments/pentaclick-lol1/matches.json', array(), 'state=open');
+
 $i = 1;
 $players = array();
+$team = array();
 $checkPlayers = array();
 foreach($answer as $f) {
+    $msg = $text;
     //Team ID #1 - $f->match->player1_id;
-    //Team ID #2 - $f->match->player2_id;
-    $q = mysql_query('SELECT `id`, `name`, `cpt_player_id` FROM `teams` WHERE `challonge_id` = '.(int)$f->match->player1_id.' OR `challonge_id` = '.(int)$f->match->player2_id);
+    //Team ID #2 - $f->match->player2_id;   
+    $q = mysql_query('SELECT `id`, `name`, `cpt_player_id` FROM `teams` WHERE (`challonge_id` = '.(int)$f->match->player1_id.' OR `challonge_id` = '.(int)$f->match->player2_id.') AND `approved` = 1 AND `deleted` = 0');
     while($r = mysql_fetch_object($q)) {
+        $team[$i] = $r->name;
         $q2 = mysql_query('SELECT `id`, `name`, `player_id` FROM `players` WHERE `team_id` = '.$r->id.' AND `approved` = 1');
         $j = 0;
         while($r2 = mysql_fetch_object($q2)) {
@@ -37,31 +35,49 @@ foreach($answer as $f) {
             $players[$i][$j]['name'] = $r2->name;
             $players[$i][$j]['player_id'] = $r2->player_id;
             
-            $checkPlayers[$i][] = $r->player_id;
+            $checkPlayers[$i][] = $r2->player_id;
             ++$j;
         }
         ++$i;
     }
     
     $answer = runAPI('/euw/v1.3/game/by-summoner/'.$players[1][0]['player_id'].'/recent', true);
-    
+    $won = '';
     foreach($answer->games as $f2) {
         if ($f2->gameType == 'CUSTOM_GAME') {
-            dump($f2);
-            foreach($f2->fellowPlayers as $f3) {
-                if (!in_array($f3->summonerId, $checkPlayers)) {
-                    
+            $q3 = mysql_query('SELECT * FROM fights WHERE game_id = '.$f2->gameId);
+            if (mysql_num_rows($q3) == 0) {
+                if ($f2->fellowPlayers) {
+                    foreach($f2->fellowPlayers as $f3) {
+                        if (!in_array($f3->summonerId, $checkPlayers)) {
+                            if ($f2->stats->win == 1) {
+                                $won = $team[1];
+                            }
+                            else {
+                                $won = $team[2];
+                            }
+                            
+                            $msg = str_replace(
+                                array('%team1%', '%team2%', '%win%'),
+                                array($team[1], $team[2], $won),
+                                $msg
+                            );
+                            mysql_query('INSERT INTO fights SET game_id = '.$f2->gameId);
+                            sendMail('max.orlovsky@gmail.com', 'PentaClick tournament - Result', $msg);
+                            break(2);
+                        }
+                    }
                 }
             }
         }
     }
     
-    //sendMail('max.orlovsky.net, pentaclickesports@gmail.com', 'PentaClick tournament - Round 1', $msg);
+    
     
     $i = 1;
     $players = array();
     $checkPlayers = array();
     
     //exit();
-    sleep(5);
+    sleep(3);
 }
