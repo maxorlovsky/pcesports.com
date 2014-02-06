884f9736f6ec2ec0748397510f77ce4fb7f7d1b2
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-02-06 13:23:50 +0000

    Email notifications in EN

--- wp-content/themes/pentaclick/emails.php
+++ wp-content/themes/pentaclick/emails.php
@@ -1,12 +1,11 @@
 <?php
-exit('waiting for end');
 if ($_GET['mailjob'] != 'runrun') {
     exit('.');
 }
 
 require_once $_SERVER['DOCUMENT_ROOT'].'/wp-config.php';
 
-$text = 'Уважаемая команда <b>%team%</b>,<br />
+/*$text = 'Уважаемая команда <b>%team%</b>,<br />
 первый день практически на носу, скоро начнутся первые бои в нашем турнире!<br />
 <br />
 В первую очередь хотелось бы рассказать как будет проходить турнир:<br />
@@ -23,7 +22,7 @@ $text = 'Уважаемая команда <b>%team%</b>,<br />
 <br />
 После того как команды достигли четверть финала, турнир возьмёт не большую паузу и будет анонсирован день финальных игр.<br />
 <br />
-PentaClick eSports.';
+PentaClick eSports.';*/
 
 /*$text = 'Dear team <b>%team%</b>,<br />
 the day almost come upon us for the first days of battles in our tournament!<br />
@@ -44,16 +43,69 @@ After all teams have reached quarterfinals, the tournament will briefly stop, an
 <br />
 PentaClick eSports.';*/
 
+$text = 'Dear team <b>%team%</b>,<br />
+Congratulations for making it to quarterfinals!<br />
+<br />
+We want to inform you that all games from quarterfinals to finals will be held this sunday, 9 feb.<br />
+Games will be streamed online, with shoutcasters on 2 chanels and different languages.<br />
+<br />
+Without further ado, we will rush in schedule:<br />
+Start at 12:00 CET/GMT+1<br />
+12:00 - 2 "top" quarter finals games will be played.<br />
+<i>DrakenDodders or Divine Desteny</i> VS LatvianSureno and Noximus Prime vs i2HARD.Gaming<br />
+13:00 - 2 "bottom" quarter finals games will be played.<br />
+Violent Gaming EU VS Bronzodia! and Æsy VS Splendid Gaming<br />
+14:00 - first semifinal game<br />
+15:00 - second semifinal game<br />
+16:00 - match for bronze 3rd place<br />
+17:00 - 1h break before finals
+18:00 to 21:00 - finals<br />
+<br />
+Of course if games will be played faster, then whole tournament and stream will go much faster then planned.<br />
+We kindly ask you to be ready to play at scheduled time because stream will be live online and we will not gonna be able to change the time. If team is unable to be on time it will be disqualified instantly.<br />
+Time limit for absence - 15 minutes.<br />
+In game time pause limit - 15 minutes.<br />
+<br />
+We wish you all good luck in your upcoming games and we hope to see great plays from you.<br />
+<br />
+PentaClick eSports.';
+
+/*$text = 'Уважаемая команда <b>%team%</b>,<br />
+Поздравляем вас. Вы достигли четвертьфинала!<br />
+<br />
+Мы хотим проинформировать вас, что все игры начиная с четвертифинала до финала будут сыграны в эту субботу, 9 фев.<br />
+Игры будут проигрыватся в онлайн стриме, с комментаторами, 2-ух канала и разных языках.<br />
+<br />
+Без дальнейших церемоний, мы перейдём к расписанию:<br />
+Старт будет в 12:00 CET/GMT+1. По Московскому времени это 15:00 (GMT+4).<br />
+12:00 - играют 2 "верхних" четвертьфиналиста.<br />
+<i>DrakenDodders или Divine Desteny</i> против LatvianSureno и Noximus Prime против i2HARD.Gaming<br />
+13:00 - играют 2 "нижних" четвертьфиналиста.<br />
+Violent Gaming EU против Bronzodia! и Æsy против Splendid Gaming<br />
+14:00 - первая полуфинальная игра<br />
+15:00 - вторая получинальная игра<br />
+16:00 - матч за бронзовое 3-тье место<br />
+17:00 - перерыв 1ч перед финалами
+18:00 до 21:00 - финалы<br />
+<br />
+Если игры будут проходить гораздо быстрее чем 1ч. то конечно же и весь турнир будет проходить быстрее чем запланированно в расписании.<br />
+Мы просим вас быть готовыми играть в назначенное время, поскольку стрим будет в лайв-трансляции и мы не сможем менять время. Если команда не сможем появится в назначенное время, она будет немедленно дисквалифицированна.<br />
+Время разрешающее на задержку перед игрой - 15 минут.<br />
+Внутри-игровая пауза - 15 минут.<br />
+<br />
+Мы желаем вам удачи в предстоящих боях и надеемся увидить от вас отличные игры.<br />
+<br />
+PentaClick eSports.';*/
+
 $q = mysql_query('SELECT name, email FROM teams WHERE approved = 1 AND deleted = 0 AND game = "lol" and tournament_id = 1');
 while ($r = mysql_fetch_object($q)) {
-    if (substr($r->email, -2) == 'ru') {
+    if (substr($r->email, -2) != 'ru') {
         $msg = str_replace(
             array('%team%'),
             array($r->name),
             $text
         );
-        
-        sendMail($r->email, 'PentaClick LoL tournament #1 - детали', $msg);
+        sendMail($r->email, 'PentaClick LoL tournament #1 - quarterfinals to finals', $msg);
         
         sleep(2);
     }
