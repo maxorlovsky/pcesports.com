ee4e5743c2b0a4053cdb248f11b7faf0ae255016
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-11-25 09:16:34 +0000

    Hotfix, time wasn't shown

--- web/pages/hearthstone/source.php
+++ web/pages/hearthstone/source.php
@@ -115,19 +115,25 @@ class hearthstone extends System
             'WHERE `game` = "hslan" AND `tournament_id` = '.(int)$this->pickedTournament.' AND `deleted` = 0 '.
             'ORDER BY `id` ASC'
         );
-        
         if ($rows) {
-            foreach($rows as $v) {
-                $tournamentTime['registration'] = date('d M Y, H:i', strtotime($v->dates_registration.' '.$v->time) + $this->data->user->timezone);
-                $tournamentTime['start'] = date('d M Y, H:i', strtotime($v->dates_start.' '.$v->time) + $this->data->user->timezone);
-            }
-        
             foreach($rows as &$v) {
                 $v->contact_info = json_decode($v->contact_info);
             }
         }
         $this->participants = $rows;
         unset($v);
+        
+        $tournamentRows = Db::fetchRows('SELECT `dates_start`, `dates_registration`, `time`, `status` '.
+            'FROM `tournaments` '.
+            'WHERE `game` = "hslan" AND '.
+            '`name` = '.(int)$this->pickedTournament.' '
+        );
+        if ($tournamentRows) {
+            foreach($tournamentRows as $v) {
+                $tournamentTime['registration'] = date('d M Y, H:i', strtotime($v->dates_registration.' '.$v->time) + $this->data->user->timezone);
+                $tournamentTime['start'] = date('d M Y, H:i', strtotime($v->dates_start.' '.$v->time) + $this->data->user->timezone);
+            }
+        }
         
         include_once _cfg('pages').'/'.get_class().'/tournament.tpl';
 	}
