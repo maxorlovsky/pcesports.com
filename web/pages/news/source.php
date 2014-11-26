9b2b7a93b363f63a5b456f2ac21c66c5e4ef4683
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-11-26 14:23:48 +0000

    Stripping tags from og description, breaking everything

--- web/pages/news/source.php
+++ web/pages/news/source.php
@@ -88,7 +88,7 @@ class news extends System
         $seo = array(
             'title' => ($news->title?$news->title.' | ':null).'News',
             'ogImg' => ($news->extension?_cfg('imgu').'/news/small-'.$news->id.'.'.$news->extension:null),
-            'ogDesc' => $news->short_english,
+            'ogDesc' => strip_tags($news->short_english),
         );
         
         return (object)$seo;
