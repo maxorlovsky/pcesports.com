4b894f11ce88e4625ebe846ec434a04d12f998eb
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2014-12-22 00:46:03 +0000

    Updating team page

--- web/pages/team/source.php
+++ web/pages/team/source.php
@@ -13,22 +13,18 @@ class team
             '`id` = 132 OR '. //arturs
             '`id` = 209 OR '. //zanko
             '`id` = 213 OR '. //aven
-            '`id` = 126 OR '. //angel-ada
-            '`id` = 220 ' //darkwing229
+            '`id` = 126 ' //angel-ada
             
         );
         
         $this->team = array(
-            1 => array('avatar' => '', 'name' => '', 'role' => 'Founder', 'contact' => 'info (at) pcesports (dot) com'),
-            213 => array('avatar' => '', 'name' => '', 'role' => 'Co-founder', 'contact' => 'aven (at) pcesports (dot) com'),
-            112 => array('avatar' => '', 'name' => '', 'role' => 'Graphic designer', 'contact' => '&nbsp;'),
-            44 => array('avatar' => '', 'name' => '', 'role' => 'Communications manager', 'contact' => 'connect (at) pcesports (dot) com'),
-            126 => array('avatar' => '', 'name' => '', 'role' => 'Community manager (VK)', 'contact' => 'bugerman21 (at) pisem (dot) net'),
-            4 => array('avatar' => '', 'name' => 'Acolent', 'role' => 'Shoutcaster', 'contact' => '&nbsp;'),
+            1 => array('avatar' => '', 'name' => '', 'role' => 'Chief Executive Officer', 'contact' => '&nbsp;'),
+            112 => array('avatar' => '', 'name' => '', 'role' => 'Chief Creative Officer', 'contact' => '&nbsp;'),
+            44 => array('avatar' => '', 'name' => '', 'role' => 'Chief Communications Officer', 'contact' => '&nbsp;'),
+            213 => array('avatar' => '', 'name' => '', 'role' => 'Co-founder', 'contact' => '&nbsp;'),
             132 => array('avatar' => '', 'name' => '', 'role' => 'Graphic designer', 'contact' => '&nbsp;'),
-            5 => array('avatar' => '', 'name' => 'Soldecroix', 'role' => 'Hearthstone judge', 'contact' => '&nbsp;'),
-            209 => array('avatar' => '', 'name' => '', 'role' => 'LoL EUNE manager', 'contact' => 'izaanko (at) pcesports (dot) com'),
-            220 => array('avatar' => '', 'name' => '', 'role' => 'Video Editor', 'contact' => '&nbsp;'),
+            209 => array('avatar' => '', 'name' => '', 'role' => 'LoL EUNE manager', 'contact' => '&nbsp;'),
+            126 => array('avatar' => '', 'name' => '', 'role' => 'Community manager (VK)', 'contact' => '&nbsp;'),
         );
         
         if ($rows) {
