7d1b79720f908a8d6fcbbcacc16fe522ee45328c
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-02-15 17:42:03 +0000

    Forgot to add tournament page

---
+++ web/pages/tournaments/index.tpl
@@ -0,0 +1,38 @@
+<section class="container page lol">
+
+<div class="left-containers">
+    <div class="block">
+        <div class="block-header-wrapper">
+            <h1 class="bordered"><?=t('tournament_list')?></h1>
+        </div>
+        
+		<? if ($this->tournamentData) {
+            foreach($this->tournamentData as $v) { ?>
+        <a class="block-content <?=(strtolower($v['status'])=='ended'?'ended-tournament':'active-tournament')?>" href="<?=_cfg('href')?>/<?=$v['link']?>">
+            <div class="left-part">
+                <div class="title"><img src="<?=_cfg('img')?>/<?=$v['game']?>-logo-small.png" /><?=$v['server']?> <?=t('tournament')?> #<?=$v['name']?></div>
+                <div class="participant_count"><?=(isset($v['teamsCount'])?$v['teamsCount']:0)?> <?=t('of')?> <?=$v['max_num']?> <?=t('participants')?></div>
+            </div>
+            
+            <div class="right-part">
+                <div class="status"><?=$v['status']?></div>
+                <div class="event-date"><?=t('event_date')?>: <?=$v['dates_start']?></div>
+                <div class="event-date"><?=t('prize_pool')?>: <?=$v['prize']?></div>
+            </div>
+            
+            <div class="mid-part">
+                <div class="clear"></div>
+            </div>
+        </a>
+        <?
+            }
+        }
+        else {
+            ?>
+            <div class="block-content">
+                <?=t('no_tournaments_registered')?>
+            </div><?
+        }
+        ?>
+    </div>
+</div>
---
+++ web/pages/tournaments/source.php
@@ -0,0 +1,130 @@
+<?php
+
+class tournaments extends System
+{
+	public $tournamentData;
+	public $teamsPlaces;
+	
+	public function __construct($params = array()) {
+		parent::__construct();
+	}
+	
+	public function getTournamentList() {
+		$rows = Db::fetchRows('SELECT * FROM `tournaments` WHERE '.
+            '(`game` = "lol" AND `name` = '.(int)$this->data->settings['lol-current-number-euw'].' AND `server` = "euw") OR '.
+            '(`game` = "lol" AND `name` = '.(int)$this->data->settings['lol-current-number-eune'].' AND `server` = "eune") OR '.
+            '(`game` = "smite" AND `name` = '.(int)$this->data->settings['smite-current-number-na'].' AND `server` = "na") OR '.
+            '(`game` = "smite" AND `name` = '.(int)$this->data->settings['smite-current-number-eu'].' AND `server` = "eu") OR '.
+            '(`game` = "hs" AND `name` = '.(int)$this->data->settings['hs-current-number'].') '.
+            'ORDER BY `id` DESC '
+        );
+        
+        if ($rows) {
+            foreach($rows as $v) {
+                $startTime = strtotime($v->dates_start.' '.$v->time);
+                $regTime = strtotime($v->dates_registration.' '.$v->time);
+                $time = $regTime;
+                
+                if ($v->server) {
+                    $checkInStatus = $this->data->settings['tournament-checkin-'.$v->game.'-'.$v->server];
+                    $checkLive = $this->data->settings['tournament-start-'.$v->game.'-'.$v->server];
+                    $checkReg = $this->data->settings['tournament-reg-'.$v->game.'-'.$v->server];
+                }
+                else {
+                    $checkInStatus = $this->data->settings['tournament-checkin-'.$v->game];
+                    $checkLive = $this->data->settings['tournament-start-'.$v->game];
+                    $checkReg = $this->data->settings['tournament-reg-'.$v->game];
+                }
+                
+                if ($checkInStatus == 1) {
+                    $v->status = t('check_in');
+                    $time = $startTime;
+                }
+                else if ($checkLive == 1) {
+                    $v->status = t('live');
+                    $time = $startTime;
+                }
+                else if ($checkReg == 1) {
+                    $v->status = t('registration');
+                }
+                else if ($v->status == 'Ended') {
+                    $v->status = t('ended');
+                }
+                else {
+                    $v->status = t('active');
+                }
+                
+                if ($v->game == 'lol') {
+                    $link = 'leagueoflegends/'.$v->server;
+                }
+                else if ($v->game == 'hs') {
+                    $link = 'hearthstone';
+                }
+                else if ($v->game == 'smite') {
+                    $link = 'smite/'.$v->server;
+                }
+                
+                $this->tournamentData[] = array(
+                    'id'	=> $v->name,
+                    'server'=> $v->server,
+                    'game'  => $v->game,
+                    'name' 	=> $v->name,
+                    'status'=> $v->status,
+                    'max_num'=> $v->max_num,
+                    'prize' => $v->prize,
+                    'dates_start'=> $v->dates_start,
+                    'link'  => $link,
+                );
+            }
+        }
+		
+        /*if ($this->tournamentData) {
+            $rows = Db::fetchRows('SELECT `tournament_id`, COUNT(`tournament_id`) AS `value`'.
+                'FROM `participants` '.
+                'WHERE `game` = "lol" AND '.
+                '`server` = "'.Db::escape($this->server).'" AND ' .
+                '`approved` = 1 AND '.
+                '`deleted` = 0 '.
+                'GROUP BY `tournament_id` '.
+                'ORDER BY `id` DESC'
+            );
+            if ($rows) {
+                foreach($rows as $v) {
+                    if ($this->tournamentData[$v->tournament_id]) {
+                        $this->tournamentData[$v->tournament_id]['teamsCount'] = $v->value;
+                    }
+                }
+            }
+        
+            $rows = Db::fetchRows('SELECT `tournament_id`, `name`, `place` '.
+                'FROM `participants` '.
+                'WHERE `game` = "lol" AND '.
+                '`server` = "'.Db::escape($this->server).'" AND '.
+                '`place` != 0 '.
+                'ORDER BY `tournament_id`, `place`'
+            );
+            
+            $previousTournamentId = 0;
+            if ($rows) {
+                foreach($rows as $v) {
+                    if ($v->tournament_id != $previousTournamentId && $this->tournamentData[$v->tournament_id]) {
+                        $this->tournamentData[$v->tournament_id]['places'][$v->place] = $v->name;
+                    }
+                }
+            }
+        }*/
+		
+		include_once _cfg('pages').'/'.get_class().'/index.tpl';
+	}
+	
+	public static function getSeo() {
+		$seo = new stdClass();
+		$seo->title = 'Pentaclick tournaments list';
+        
+		return $seo;
+	}
+	
+	public function showTemplate() {
+		$this->getTournamentList();
+	}
+}
