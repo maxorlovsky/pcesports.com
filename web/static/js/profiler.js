b1ae12376a0e95ae419792ac03278a94384eec4c
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-03-28 11:27:46 +0000

    Removing console logs

--- web/static/js/profiler.js
+++ web/static/js/profiler.js
@@ -116,12 +116,6 @@ var profiler = {
                     checkHeight = parseInt($('.chat-content').prop('scrollHeight'));
                     
                     currentContent = $('.chat-content').html().replace(/&lt;/g, '&#60;').replace(/&gt;/g, '&#62;').replace(/&amp;/g, '&');
-                    console.log(currentContent);
-                    console.log(answer[1]);
-                    console.log(profiler.stripTags(currentContent));
-                    console.log(profiler.stripTags(answer[1]));
-                    console.log(escape(profiler.stripTags(currentContent)));
-                    console.log(escape(profiler.stripTags(answer[1])));
                     if (escape(profiler.stripTags(currentContent)) != escape(profiler.stripTags(answer[1])) && profiler.chatStart == 1) {
                         //$('#ping').play();
                         document.getElementById('ping').play();
