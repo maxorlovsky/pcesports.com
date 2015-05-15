5fd642c5b964c60e11825745c73fa055816c4e5f
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-05-15 12:28:35 +0000

    Changing limit for test

--- web/classes/cron.php
+++ web/classes/cron.php
@@ -6,7 +6,7 @@ class Cron extends System {
     }
 
     public function emailSender() {
-        $limit = 10;
+        $limit = 50;
 
         $row = Db::fetchRow(
             'SELECT * FROM `subscribe_sender` '.
