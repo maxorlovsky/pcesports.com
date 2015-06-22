0d155ed447b78fab484b9193daf063fc9422727d
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-06-22 15:42:46 +0000

    Small change

--- cms/modules/participantsHs/source.php
+++ cms/modules/participantsHs/source.php
@@ -9,15 +9,15 @@ class ParticipantsHs
 	function __construct($params = array()) {
 		$this->system = $params['system'];
         
+        $row = Db::fetchRow('SELECT `value` FROM `tm_settings` WHERE `setting` = "hs-current-number-s1"');
+        $this->currentTournament = $row->value;
+        
         //Enable/disable
         if (isset($params['var1']) && $params['var1'] == 'able' && isset($params['var2'])) {
         	$this->able($params['var2']);
         	//redirect
         	go(_cfg('cmssite').'/#participantsHs');
         }
-        
-        $row = Db::fetchRow('SELECT `value` FROM `tm_settings` WHERE `setting` = "hs-current-number-s1"');
-        $this->currentTournament = $row->value;
         
 		$this->participants = Db::fetchRows(
             'SELECT `id`, `name`, `email`, `verified` '.
@@ -42,14 +42,29 @@ class ParticipantsHs
     
     protected function able($id) {
     	$id = (int)$id;
-    	$row = Db::fetchRow('SELECT `name`, `verified` FROM `participants` WHERE `id` = '.$id.' AND `tournament_id` = '.(int)$this->currentTournament.' AND `game` = "hs" LIMIT 1');
+    	$row = Db::fetchRow(
+            'SELECT `name`, `verified` FROM `participants` '.
+            'WHERE `id` = '.$id.' AND '.
+            '`server` = "s1" AND '.
+            '`tournament_id` = '.(int)$this->currentTournament.' AND '.
+            '`game` = "hs" '.
+            'LIMIT 1'
+        );
     	if ($row->verified == 1) {
     		$enable = 0;
     	}
     	else {
     		$enable = 1;
     	}
-    	Db::query('UPDATE `participants` SET `verified` = '.$enable.' WHERE `id` = '.$id.' AND `tournament_id` = '.(int)$this->currentTournament.' AND `game` = "hs" LIMIT 1');
+    	Db::query(
+            'UPDATE `participants` '.
+            'SET `verified` = '.$enable.' '.
+            'WHERE `id` = '.$id.' AND '.
+            '`server` = "s1" AND '.
+            '`tournament_id` = '.(int)$this->currentTournament.' AND '.
+            '`game` = "hs" '.
+            'LIMIT 1'
+        );
 
     	if ($enable == 1) {
     		$this->system->log('Verify HS participant <b>('.$row->name.')</b>', array('module'=>get_class(), 'type'=>'enabling'));
