5ad846033d6abdd568004efaa3cee31f3eb39cd5
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-07-20 07:07:05 +0000

    Testing https stuff

--- cms/classes/system.php
+++ cms/classes/system.php
@@ -20,15 +20,14 @@ class System
             Db::connect();
     	}
         
-        //As soon as DB class is enabled, checking https staru
+        //As soon as DB class is enabled, checking https status
         $row = Db::fetchRow('SELECT `value` FROM `tm_settings` WHERE `setting` = "https" LIMIT 1');
         //Checking if https always enabled and if user is on http, then redirecting to https
-        dump($row->value);
-        dump(extension_loaded('openssl'));
         dump($_SERVER);
         if ($row->value == 1 && extension_loaded('openssl')
             && ( (!isset($_SERVER['HTTPS']) || $_SERVER['HTTPS'] != 'on') || (!isset($_SERVER['HTTP_X_FORWARDED_PROTO']) || $_SERVER['HTTP_X_FORWARDED_PROTO'] != 'https') )
             ) {
+                echo 'trying to redirect';
             go(str_replace('http', 'https', _cfg('cmssite')));
         }
 
