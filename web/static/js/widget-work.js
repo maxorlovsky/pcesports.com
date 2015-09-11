f1c68e8ac575a54aa32dbd82a28df472fe51a6ff
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2015-09-11 07:09:03 +0000

    Updating widget core functionality. Nesting, oop, $ to jQuery

--- web/static/js/widget-work.js
+++ web/static/js/widget-work.js
@@ -1,113 +1,117 @@
 (function(){
     'use strict';
 
-if (window.jQuery) {
+    if (window.jQuery) {
+        jQuery('#pce-widget').ready(function() {
+            widget.initiate();
+        });
+    }
+    else {
+        console.log('jQuery not found, shutting down');
+    }
 
-    $('#pce-widget').ready(function() {
-        var widget = $('#pce-widget');
-        var iframe;
-        var platform;
-        var parentUrl;
-        var get = {};
-        
-        if (window.location.href.indexOf('dev') != -1) {
-            platform = 'http://dev.';
-        }
-        else if (window.location.href.indexOf('test') != -1) {
-            platform = 'http://test.';
-        }
-        else {
-            platform = 'https://www.';
-        }
-        
-        if (window.location.hostname.indexOf('skillz') != -1) {
-            parentUrl = platform+'pcesports.com/widget/skillzhs';
-        }
-        else if (window.location.hostname.indexOf('unicon') != -1) {
-            parentUrl = platform+'pcesports.com/widget/uniconhs';
-        }
-        else if (window.location.hostname.indexOf('bnb') != -1) {
-            parentUrl = platform+'pcesports.com/widget/bnb';
-        }
-        else if (window.location.hostname.indexOf('pcesports') != -1) {
-            if (window.location.href.indexOf('skillz') != -1) {
-                parentUrl = platform+'pcesports.com/widget/skillzhs';
+    var widget = {
+        initiate: function() {
+            var widgetElement = jQuery('#pce-widget');
+            var iframe;
+            var platform;
+            var parentUrl;
+            var get = {};
+            
+            if (window.location.href.indexOf('dev') != -1) {
+                platform = 'http://dev.';
             }
-            else if (window.location.href.indexOf('unicon') != -1) {
-                parentUrl = platform+'pcesports.com/widget/uniconhs';
-            }
-            else if (window.location.href.indexOf('bnb') != -1) {
-                parentUrl = platform+'pcesports.com/widget/bnb';
+            else if (window.location.href.indexOf('test') != -1) {
+                platform = 'http://test.';
             }
             else {
-                console.log('Please specify platform');
+                platform = 'https://www.';
+            }
+            
+            if (window.location.hostname.indexOf('skillz') != -1) {
+                parentUrl = platform+'pcesports.com/widget/skillzhs';
+            }
+            else if (window.location.hostname.indexOf('unicon') != -1) {
+                parentUrl = platform+'pcesports.com/widget/uniconhs';
+            }
+            else if (window.location.hostname.indexOf('bnb') != -1) {
+                parentUrl = platform+'pcesports.com/widget/bnb';
+            }
+            else if (window.location.hostname.indexOf('pcesports') != -1) {
+                if (window.location.href.indexOf('skillz') != -1) {
+                    parentUrl = platform+'pcesports.com/widget/skillzhs';
+                }
+                else if (window.location.href.indexOf('unicon') != -1) {
+                    parentUrl = platform+'pcesports.com/widget/uniconhs';
+                }
+                else if (window.location.href.indexOf('bnb') != -1) {
+                    parentUrl = platform+'pcesports.com/widget/bnb';
+                }
+                else {
+                    console.log('Please specify platform');
+                    return false;
+                }
+            }
+            else {
+                console.log('Your website is not allowed to use Pentaclick eSports widget');
                 return false;
             }
-        }
-        else {
-            console.log('Your website is not allowed to use Pentaclick eSports widget');
-            return false;
-        }
-        
-        if (window.location.href.indexOf('participant') != -1) {
-            breakdownGlobal = window.location.href.split('&');
-            delete breakdownGlobal[0];
-            $.each(breakdownGlobal, function(k, v) {
-                if (v != undefined) {
-                    breakdown = v.split('=');
-                    if (breakdown[1] != undefined) {
-                        get[breakdown[0]] = breakdown[1];
+            
+            if (window.location.href.indexOf('participant') != -1) {
+                breakdownGlobal = window.location.href.split('&');
+                delete breakdownGlobal[0];
+                jQuery.each(breakdownGlobal, function(k, v) {
+                    if (v != undefined) {
+                        breakdown = v.split('=');
+                        if (breakdown[1] != undefined) {
+                            get[breakdown[0]] = breakdown[1];
+                        }
                     }
-                }
+                });
+                parentUrl += '/participant/'+get.participant+'/'+get.link;
+            }
+
+            if (widgetElement.length <= 0) {
+                console.log('Widget frame not found, shutting down');
+                return false;
+            }
+
+            //widget-css
+            widgetElement.css({
+                width: '100%',
+                margin: '0 auto'
             });
-            parentUrl += '/participant/'+get.participant+'/'+get.link;
-        }
 
-        if (widget.length <= 0) {
-            console.log('Widget frame not found, shutting down');
-            return false;
-        }
-
-        //widget-css
-        widget.css({
-            width: '100%',
-            margin: '0 auto'
-        });
-
-        widget.html('<iframe />');
-        
-        iframe = widget.find('iframe');
-        iframe.css({
-            width: '100%',
-            border: '0',
-        });
-        iframe.attr('src', parentUrl);
-        
-        if (window.addEventListener) {
-            window.addEventListener("message", fetchFrameMessage, false);
-        } else {
-            window.attachEvent("onmessage", fetchFrameMessage);
-        }
-    });
-
-    function fetchFrameMessage(event) {
-        var data = '';
-        
-        if (!event) {
-            console.log('No event data sent');
-            return false;
-        }
-        
-        if (event.data) {
-            data = event.data.split('=');
-            if (data[0] == 'height') {
-                $('#pce-widget').find('iframe').height(data[1]);
+            widgetElement.html('<iframe />');
+            
+            iframe = widgetElement.find('iframe');
+            iframe.css({
+                width: '100%',
+                border: '0',
+            });
+            iframe.attr('src', parentUrl);
+            
+            if (window.addEventListener) {
+                window.addEventListener("message", widget.fetchFrameMessage, false);
+            } else {
+                window.attachEvent("onmessage", widget.fetchFrameMessage);
+            }
+        },
+        fetchFrameMessage: function(event) {
+            var data = '';
+            
+            if (!event) {
+                console.log('No event data sent');
+                return false;
+            }
+            
+            if (event.data) {
+                data = event.data.split('=');
+                if (data[0] == 'height') {
+                    jQuery('#pce-widget').find('iframe').height(data[1]);
+                }
             }
         }
     };
-}
-else {
-    console.log('jQuery not found, shutting down');
-}
 
 })();
