499d57d8ac52bd461748d615c23d7ee66e5e77c5
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2016-03-29 06:00:31 +0000

    Remove skillz from partners page

--- web/pages/partners/source.php
+++ web/pages/partners/source.php
@@ -11,11 +11,6 @@ class partners extends system
     			'link' => 'http://www.unicon.lv',
     			'text' => 'UniCon - one of the first multingenre conventions in Eastern Europe. The event for comic, anime, book, movie, cosplay, crafting and many other fans. We\'ve been working closely with UniCon since 2014, helping out with eSports events and holding our own events @ UniCon'
 			),
-			'skillz' => array(
-    			'logo' => _cfg('img').'/partners/skillz.png',
-    			'link' => 'http://skillz.lv',
-    			'text' => 'Skillz - gaming organization, the main focus of which is to develop e-Sports in Latvia and in Baltics countries. Together with skillz we were able to made few co-op tournaments and exchanging informations almost every time'
-			),
 			'lesf' => array(
     			'logo' => _cfg('img').'/partners/lesf.png',
     			'link' => 'http://www.lesf.lv',
