a328a9507c85f615510442f529d6c17668bcf4d1
max <max@75669341-5371-443d-b72f-627b08b5ad0c>
2016-05-02 16:08:06 +0000

    Updating hs-stream files. Add node support to update on all screens simultaniously. Add ff.js that will be run on stream machine to update files that need to connecto hs.js somehow. Add banned icons that were missing

---
+++ tools/hs-stream/ff.js
@@ -0,0 +1,19 @@
+//for files
+const net = require("net");
+
+// Create a socket (client) that connects to the server
+var socket = new net.Socket();
+socket.connect(3000, 'localhost', function () {
+    console.log("Client: Connected to server");
+});
+
+// Let's handle the data we get from the server
+socket.on("data", function (data) {
+	console.log(data);
+    /*data = JSON.parse(data);
+    console.log("Response from server: %s", data.response);
+    // Respond back
+    socket.write(JSON.stringify({ response: "Hey there server!" }));
+    // Close the connection
+    socket.end();*/
+});
--- tools/hs-stream/hs.js
+++ tools/hs-stream/hs.js
@@ -2,27 +2,27 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var fs = require('fs');
-//index = fs.readFileSync(__dirname + '/hs.html');
-
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/hs.html');;
-});
+var request = require('request');
 
 io.on('connection', function(socket){
   	io.emit('status', 'Connected');
-  	//document.getElementById('status').value = 'Connected';
+  	io.emit('updateUserList', stream.usersFromServer);
+  	io.emit('userSelectedData', stream.users);
 
 	socket.on('disconnect', function(){
 		io.emit('status', 'Disconnected');
 	});
 
 	socket.on('updateIcons', function(data){
-		console.log(data);
 		var pathToIcons = 'icons';
 		var pathToPlayerFolder = data[0];
 		var playerFileName = [];
 
-		fs.writeFile(pathToPlayerFolder+'/name.txt', data[1], function(err) {
+		stream.users[pathToPlayerFolder] = data;
+
+		io.emit('userSelectedData', stream.users);
+
+		/*fs.writeFile(pathToPlayerFolder+'/name.txt', data[1], function(err) {
 			if (err) {
 				return console.log(err);
 			}
@@ -33,11 +33,94 @@ io.on('connection', function(socket){
 			fs.createReadStream(pathToIcons+'/'+fileName).pipe(fs.createWriteStream(pathToPlayerFolder+'/'+i+'.png'));
 		}
 
-		fileName = data[8]+'-banned.png';
-		fs.createReadStream(pathToIcons+'/'+fileName).pipe(fs.createWriteStream(pathToPlayerFolder+'/b.png'));
+		if (data[8] != 'undefined') {
+			fileName = data[8]+'-banned.png';
+			fs.createReadStream(pathToIcons+'/'+fileName).pipe(fs.createWriteStream(pathToPlayerFolder+'/b.png'));
+		}*/
+	});
+
+	socket.on('cleanUser', function(num) {
+		stream.users[num] = null;
+		io.emit('userSelectedData', stream.users);
 	});
 });
 
-http.listen(3000, function(){
+//Initiate
+http.listen(3000, function() {
+	stream.fetchUsers();
+
+	setInterval(stream.fetchUsers, 5000); //rerun to fetch list every 15 seconds, it's localhost so should be fine
+
 	console.log('Running');
 });
+
+var stream = {
+	//variables
+	users: [null, null, null],
+	usersFromServer: {},
+	hsListLink: 'http://dev.pcesports.com/run/hslist/haosdi012',
+	request: null,
+	heroes: [
+        '',
+        'warrior',
+        'hunter',
+        'mage',
+        'warlock',
+        'shaman',
+        'rogue',
+        'druid',
+        'paladin',
+        'priest'
+    ],
+
+	//functions
+	fetchUsers: function() {
+		if (stream.request !== null) {
+			stream.request.abort();
+		}
+
+		stream.request = request(stream.hsListLink, function (error, response, body) {
+			if (error || response.statusCode !== 200) {
+				console.log('Call request error');
+				return false;
+			}
+
+			if (JSON.stringify(stream.usersFromServer) !== JSON.stringify(body)) {
+				//List updated, then giving this part to frontend and updating the link
+
+				//Update global variable
+				stream.usersFromServer = body;
+
+				//Sending data to frontend
+				io.emit('updateUserList', stream.usersFromServer);
+
+				var userDataUpdateRequired = false;
+
+				//Classes/Bans updates, figuring out and updating current users data
+				for(i=1; i<=2; ++i) {
+					if (stream.users[i] !== null) {
+						obj = JSON.parse(body);
+						for (var k in obj) {
+							v = obj[k];
+							if (v.name == stream.users[i][1]) {
+								stream.users[i][2] = stream.heroes[v.contact_info.hero1];
+								stream.users[i][3] = stream.heroes[v.contact_info.hero2];
+								stream.users[i][4] = stream.heroes[v.contact_info.hero3];
+								stream.users[i][8] = stream.heroes[v.contact_info.ban];
+
+								userDataUpdateRequired = true;
+							}
+						}
+					}
+				}
+
+				//Sending data to frontend, only if bans or classes were updated for that user
+				if (userDataUpdateRequired === true) {
+					io.emit('userSelectedData', stream.users);
+				}
+			}
+
+			stream.request = null;
+		});
+	}
+}
