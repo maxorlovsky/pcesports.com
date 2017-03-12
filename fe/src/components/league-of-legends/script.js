aa1dd15527986326ba382092e9770b9dd9dc7cfb
Maxtream <max.orlovsky@gmail.com>
2017-03-12 23:33:06 +0100

    Add tournament list without styles for now

--- fe/src/components/league-of-legends/script.js
+++ fe/src/components/league-of-legends/script.js
@@ -1,60 +1,22 @@
 const LolTournaments = {
     template: '#lol-template',
     data: function() {
-        const games = [
-            {
-                gameName: 'League of Legends',
-                cssClass: 'game-lol',
-                tournamentsNumbers: 0,
-                link: '../league-of-legends/'
-            },
-            {
-                gameName: 'Hearthstone',
-                cssClass: 'game-hs',
-                tournamentsNumbers: 0,
-                link: '../hearthstone'
-            },
-            /*{
-                gameName: 'Counter-Strike: GO',
-                cssClass: 'game-csgo',
-                tournamentsNumbers: 0
-            },
-            {
-                gameName: 'Overwatch',
-                cssClass: 'game-ow',
-                tournamentsNumbers: 0
-            },
-            {
-                gameName: 'Heroes of the Storm',
-                cssClass: 'game-hots',
-                tournamentsNumbers: 0
-            },
-            {
-                gameName: 'Smite',
-                cssClass: 'game-smite',
-                tournamentsNumbers: 0
-            }*/
-        ];
-
         return {
             loading: true,
-            posts: 0,
-            games: games
+            games: {}
         };
     },
     created: function() {
         var self = this;
 
-        this.loading = true;
-
-        axios.get('https://api.pcesports.com/wp/wp-json/wp/v2/posts/?per_page=4')
+        axios.get('https://api.pcesports.com/wp/wp-json/pce-api/tournaments/?game=lol&limit=20')
         .then(function (response) {
-            self.posts = response.data;
+            self.games = response.data;
 
-            for (let i = 0; i < self.posts.length; ++i) {
+            /*for (let i = 0; i < self.posts.length; ++i) {
                 let date = (new Date(self.posts[i].date));
                 self.posts[i].date = date.toLocaleString('en-us', { month: "short" })+'<br />'+date.getDate();
-            }
+            }*/
 
             self.loading = false;
         });
