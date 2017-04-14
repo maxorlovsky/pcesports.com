82198d3f5e401279a442b0b2434adf6f36b37439
Maxtream <max.orlovsky@gmail.com>
2017-04-14 11:05:44 +0200

    Implementing local storage for main templates, main menu and home blog posts for 30min or for version update

--- fe/src/components/home/script.js
+++ fe/src/components/home/script.js
@@ -50,17 +50,27 @@ const Home = {
 
         this.loading = true;
 
-        axios.get('https://api.pcesports.com/wp/wp-json/wp/v2/posts/?per_page=4')
-        .then(function (response) {
-            self.posts = response.data;
+        const checkStorage = pce.storage('get', 'blogs-posts');
+        
+        if (checkStorage) {
+            this.posts = checkStorage;
+            this.loading = false;
+        }
+        else {
+            axios.get('https://api.pcesports.com/wp/wp-json/wp/v2/posts/?per_page=4')
+            .then(function (response) {
+                self.posts = response.data;
 
-            for (let i = 0; i < self.posts.length; ++i) {
-                let date = (new Date(self.posts[i].date));
-                self.posts[i].date = date.toLocaleString('en-us', { month: "short" })+'<br />'+date.getDate();
-            }
+                for (let i = 0; i < self.posts.length; ++i) {
+                    let date = (new Date(self.posts[i].date));
+                    self.posts[i].date = date.toLocaleString('en-us', { month: "short" })+'<br />'+date.getDate();
+                }
 
-            self.loading = false;
-        });
+                pce.storage('set', 'blogs-posts', self.posts);
+
+                self.loading = false;
+            });
+        }
 
         document.title = document.title + ' - Home';
     }
--- fe/src/globals.js
+++ fe/src/globals.js
@@ -6,3 +6,63 @@ const dynamicTemplates = {
     ga: document.createElement('script'),
     sideMenu: document.createElement('script')
 };
+
+const pce = {
+    version: '2.3.0',
+    // Local storage
+    storage: (func, key, json) => {
+        // setItem
+        if (func === 'set') {
+            // If any parameter is empty, we don't do anything
+            if (!func || !key || !json) {
+                return false;
+            }
+
+            let saveData = {
+                data: json,
+                time: new Date().getTime()
+            };
+
+            localStorage.setItem(key, JSON.stringify(saveData));
+        } else if (func === 'get') {
+            // If it's a development environment, we ignoring localStorage cache
+            if (location.href.indexOf('dev') !== -1) {
+                return false;
+            }
+
+            // Check if there is something for specified key
+            if (!localStorage.getItem(key)) {
+                return false;
+            }
+
+            let returnValue = JSON.parse(localStorage.getItem(key));
+
+            // If more than 30 min, cleanup
+            if ((returnValue.time * 1800000) <= new Date().getTime()) {
+                pce.storage('remove', key);
+            }
+
+            return returnValue.data;
+        } else if (func === 'remove') {
+            localStorage.removeItem(key);
+        } else {
+            return false;
+        }
+
+        return true;
+    },
+    storageCacheBuster: () => {
+        let storagesKeys = ['structure-data', 'blogs-posts'];
+
+        // If version was bumped, we might still use outdated localStorage data, doing full cleanup
+        if (localStorage.getItem('version') !== pce.version) {
+            localStorage.clear();
+            // Saving version to not cleaup everything again until the next bump
+            localStorage.setItem('version', pce.version);
+        }
+
+        return true;
+    }
+};
+
+pce.storageCacheBuster();
