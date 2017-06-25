12a14c65f57debca94147f91dd5fbdad1b89f178
Maxtream <max.orlovsky@gmail.com>
2017-06-25 12:32:42 +0200

    feat(axios-to-fetch): move to fetch call, instead of axios as this is native
    # Conflicts:
    #	fe/src/main.js

--- fe/src/main.js
+++ fe/src/main.js
@@ -74,9 +74,10 @@ router.beforeEach((to, from, next) => {
     // Loading html template for component
     let element = document.getElementById('template-holder');
 
-    axios.get('/dist/html/' + to.meta.template + '.html')
-    .then(function(template) {
-        element.innerHTML = template.data;
+    fetch('/dist/html/' + to.meta.template + '.html')
+    .then(res => res.text())
+    .then((template) => {
+        element.innerHTML = template;
 
         next();
     });
@@ -96,39 +97,34 @@ if (checkStorage) {
     loadApp(checkStorage.menu);
 }
 else {
-    axios.all([
-        axios.get('/dist/html/header.html'),
-        axios.get('/dist/html/footer.html'),
-        axios.get('/dist/html/event-item.html'),
-        //axios.get('/dist/html/events-filters.html'),
-        axios.get('/dist/html/ga.html'),
-        axios.get('/dist/html/left-side-menu.html'),
-        axios.get('/dist/html/right-side-menu.html'),
-        axios.get('/dist/html/login.html'),
-        axios.get('https://api.pcesports.com/wp/wp-json/pce-api/menu')
-    ])
-    .then(axios.spread(function (
-        headerTemplate,
-        footerTemplate,
-        eventItemTemplate,
-        gaTemplate,
-        leftSideMenuTemplate,
-        rightSideMenuTemplate,
-        loginTemplate,
-        menuData) {
-        dynamicTemplates.header.appendChild(document.createTextNode(headerTemplate.data));
-        dynamicTemplates.footer.appendChild(document.createTextNode(footerTemplate.data));
-        dynamicTemplates.eventItem.appendChild(document.createTextNode(eventItemTemplate.data));
-        //dynamicTemplates.eventsFilters.appendChild(document.createTextNode(eventsFiltersTemplate.data));
-        dynamicTemplates.ga.appendChild(document.createTextNode(gaTemplate.data));
-        dynamicTemplates.login.appendChild(document.createTextNode(loginTemplate.data));
-        dynamicTemplates.leftSideMenu.appendChild(document.createTextNode(leftSideMenuTemplate.data));
-        dynamicTemplates.rightSideMenu.appendChild(document.createTextNode(rightSideMenuTemplate.data));
+    const urls = [
+        '/dist/html/header.html',
+        '/dist/html/footer.html',
+        '/dist/html/event-item.html',
+        '/dist/html/ga.html',
+        '/dist/html/side-menu.html',
+        'https://api.pcesports.com/wp/wp-json/pce-api/menu'
+    ];
 
-        let returnMenu = {};
-        if (menuData.data) {
-            for (let value of menuData.data) {
-                if (value.menu_item_parent == '0') {
+    const grabContent = url => fetch(url)
+    .then(res => res.text())
+    .then(html => {
+        return html;
+    });
+
+    Promise.all(urls.map(grabContent))
+    .then((response) => {
+        dynamicTemplates.header.appendChild(document.createTextNode(response[0]));
+        dynamicTemplates.footer.appendChild(document.createTextNode(response[1]));
+        dynamicTemplates.eventItem.appendChild(document.createTextNode(response[2]));
+        dynamicTemplates.ga.appendChild(document.createTextNode(response[3]));
+        dynamicTemplates.sideMenu.appendChild(document.createTextNode(response[4]));
+
+        const returnMenu = {};
+        if (response[5]) {
+            response[5] = JSON.parse(response[5]);
+            for (let value of response[5]) {
+                if (value.menu_item_parent === '0') {
                     returnMenu['link-' + value.ID] = {
                         'title': value.title,
                         'url': (value.url?value.url:''),
@@ -137,8 +133,7 @@ else {
                         'slug': value.post_name,
                         'sublinks': {}
                     };
-                }
-                else {
+                } else {
                     returnMenu['link-' + value.menu_item_parent].sublinks['sublink-' + value.ID] = {
                         'title': value.title,
                         'url': value.url.replace('http://', ''),
@@ -152,13 +147,11 @@ else {
 
         let store = {
             templates: {
-                header: headerTemplate.data,
-                footer: footerTemplate.data,
-                eventItem: eventItemTemplate.data,
-                ga: gaTemplate.data,
-                leftSideMenu: leftSideMenuTemplate.data,
-                rightSideMenu: rightSideMenuTemplate.data,
-                login: loginTemplate.data
+                header: response[0],
+                footer: response[1],
+                eventItem: response[2],
+                ga: response[3],
+                sideMenu: response[4]
             },
             menu: returnMenu
         };
@@ -166,7 +159,10 @@ else {
         pce.storage('set', 'structure-data', store);
 
         loadApp(returnMenu);
-    }));
+    })
+    .catch((error) => {
+        console.log('Error fetching main resources: ' + error);
+    });
 }
 
 function loadApp(menu) {
@@ -205,10 +201,10 @@ function loadApp(menu) {
                 }
             });
 
-            /*setTimeout(() => {
-                console.log('should run at the end');
-                window.prerenderReady = true;
-            }, 3000);*/
+            //setTimeout(() => {
+            //    console.log('should run at the end');
+            //    window.prerenderReady = true;
+            //}, 3000);
         },
         methods: {
             burgerMenu: function() {
