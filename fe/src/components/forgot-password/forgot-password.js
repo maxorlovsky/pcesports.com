692e29518f1496d4bc86856a6d92175119290eba
Maxtream <max.orlovsky@gmail.com>
2017-07-17 23:38:01 +0200

    feat(forgot-password): add recaptcha to password restoration form

--- fe/src/components/forgot-password/forgot-password.js
+++ fe/src/components/forgot-password/forgot-password.js
@@ -4,14 +4,24 @@ Vue.component('forgot-password', {
         return {
             login: '',
             restoreError: '',
+            restoreSuccess: '',
+            rcapt_id: 0,
+            captchaKey: '6LcwJ_8SAAAAAL2SgH-NYduvEp9DLUlndHrlMs7Z',
             loading: false
         };
+    },
+    mounted() {
+        if (window.grecaptcha) {
+            this.rcapt_id = grecaptcha.render( document.getElementById('fp-g-recaptcha'), { sitekey : this.captchaKey });
+        }
     },
     methods: {
         submit: function() {
             let self = this;
 
             this.loading = true;
+            this.restoreSuccess = '';
+            this.restoreError = '';
 
             if (!this.login) {
                 this.restoreError = 'Please fill in the form';
@@ -19,18 +29,18 @@ Vue.component('forgot-password', {
                 return false;
             }
 
-            axios.post('http://dev.api.pcesports.com/forgot-password', {
+            axios.post(`${pce.apiUrl}/forgot-password`, {
                 login: this.login,
-                pass: this.pass
+                captcha: grecaptcha.getResponse(this.rcapt_id)
             })
             .then(function (response) {
-                pce.storage('set', 'token', response.data, 604800000); // 7 days
-                self.$parent.$parent.login();
+                self.restoreSuccess = response.data.message;
                 self.loading = false;
             })
             .catch(function (error) {
-                self.loginError = error.response.data.message;
+                self.restoreError = error.response.data.message;
                 self.loading = false;
+                grecaptcha.reset();
             });
         },
         openLoginMenu: function() {
