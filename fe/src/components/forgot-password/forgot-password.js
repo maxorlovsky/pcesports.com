e22cbd1ef63350f2fcb3e92f9111227fb5580625
Maxtream <max.orlovsky@gmail.com>
2017-07-18 23:23:55 +0200

    feat(forgot-password): add code approach and not link to reset password

--- fe/src/components/forgot-password/forgot-password.js
+++ fe/src/components/forgot-password/forgot-password.js
@@ -2,17 +2,25 @@ Vue.component('forgot-password', {
     template: dynamicTemplates.forgotPassword,
     data: function() {
         return {
-            login: '',
             restoreError: '',
             restoreSuccess: '',
-            rcapt_id: 0,
+            captchaId: 0,
             captchaKey: '6LcwJ_8SAAAAAL2SgH-NYduvEp9DLUlndHrlMs7Z',
-            loading: false
+            loading: false,
+            showForm: 1,
+            form: {
+                login: ''
+            },
+            resetForm: {
+                code: '',
+                pass: '',
+                repeatPass: ''
+            }
         };
     },
     mounted() {
         if (window.grecaptcha) {
-            this.rcapt_id = grecaptcha.render( document.getElementById('fp-g-recaptcha'), { sitekey : this.captchaKey });
+            this.captchaId = grecaptcha.render( document.getElementById('fp-g-recaptcha'), { sitekey : this.captchaKey });
         }
     },
     methods: {
@@ -23,29 +31,67 @@ Vue.component('forgot-password', {
             this.restoreSuccess = '';
             this.restoreError = '';
 
-            if (!this.login) {
+            if (!this.form.login) {
                 this.restoreError = 'Please fill in the form';
                 this.loading = false;
                 return false;
             }
 
             axios.post(`${pce.apiUrl}/forgot-password`, {
-                login: this.login,
-                captcha: grecaptcha.getResponse(this.rcapt_id)
+                login: this.form.login,
+                captcha: grecaptcha.getResponse(this.captchaId)
             })
             .then(function (response) {
                 self.restoreSuccess = response.data.message;
+                self.showForm = 2;
+                self.loading = false;
+                self.checkCaptcha();
+            })
+            .catch(function (error) {
+                self.restoreError = error.response.data.message;
+                self.loading = false;
+                self.checkCaptcha();
+            });
+        },
+        submitReset: function() {
+            let self = this;
+
+            this.loading = true;
+            this.restoreSuccess = '';
+            this.restoreError = '';
+
+            if (!this.resetForm.code || !this.resetForm.pass || !this.resetForm.repeatPass) {
+                this.restoreError = 'Please fill in the form';
+                this.loading = false;
+                return false;
+            }
+
+            axios.post(`${pce.apiUrl}/forgot-password/reset`, {
+                login: this.form.login,
+                code: this.resetForm.code,
+                pass: this.resetForm.pass,
+                repeatPass: this.resetForm.repeatPass
+            })
+            .then(function (response) {
+                self.restoreSuccess = response.data.message;
+                self.showForm = 0;
                 self.loading = false;
             })
             .catch(function (error) {
                 self.restoreError = error.response.data.message;
                 self.loading = false;
-                grecaptcha.reset();
             });
         },
         openLoginMenu: function() {
             this.$parent.$parent.rightSideMenuForm = 'login';
             this.$emit('right-menu');
+        },
+        checkCaptcha: function() {
+            if (!document.getElementById('fp-g-recaptcha').innerHTML) {
+                this.captchaId = grecaptcha.render( document.getElementById('fp-g-recaptcha'), { sitekey : this.captchaKey });
+            } else {
+                grecaptcha.reset();
+            }
         }
     }
 });
