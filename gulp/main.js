afb112c2beeb0c56f47534324a11bc82e47dd290
Maxtream <max.orlovsky@gmail.com>
2018-03-04 17:57:36 +0100

    chore(main.js-gulp): remove clean from release command

--- gulp/main.js
+++ gulp/main.js
@@ -3,13 +3,29 @@ const runSequence = require('run-sequence');
 
 require('require-dir')('./tasks');
 
+let release = false;
+
+if (process.argv.indexOf("--release") > -1) {
+    release = true;
+}
+
 gulp.task('build', (cb) => {
-	return runSequence(
-		'clean',
-		'lint:script',
-		'copy',
-		['styles', 'scripts'],
-		'bump-ver',
-		'minify',
-	cb);
+	if (release) {
+		return runSequence(
+			'lint:script',
+			'copy',
+			['styles', 'scripts'],
+			'bump-ver',
+			'minify',
+		cb);
+	} else {
+		return runSequence(
+			'clean',
+			'lint:script',
+			'copy',
+			['styles', 'scripts'],
+			'bump-ver',
+			'minify',
+		cb);
+	}
 });
