2e06136c21505e09b00c89aab60bd3983463d7b7
Max Orlovsky <max.orlovsky@gmail.com>
2018-04-05 16:09:16 +0200

    feat(change-password): move to vue single file structure

--- fe/src/pages/change-password/change-password.vue
+++ fe/src/pages/change-password/change-password.vue
@@ -1,8 +1,57 @@
-const ChangePassword = {
-    template: '#change-password-template',
+<template>
+<div class="profile">
+    <div class="block">
+        <div class="block-content semi-widths">
+            <loading v-if="loading"></loading>
+
+            <section class="profile-wrapper" v-if="!loading">
+                <h4>Change Password</h4>
+
+                <div class="alert alert-danger" v-if="formError" v-html="formError"></div>
+
+                <form method="post" v-on:submit.prevent="submit()">
+                    <div class="heading-text">Here you can change your password. Keep in mind that the only strict rule that we ask is minimum password length is 6 characters. But we do advice you to pick harder password using upper case and lower case letters.</div>
+
+                    <div class="input-wrapper input-group form-group"
+                        :class="{ error: errorClasses.oldPass }">
+                        <div class="input-group-addon fa fa-lock"></div>
+                        <input class="form-control" type="password" v-model="form.oldPass" placeholder="Current password" />
+                    </div>
+
+                    <div class="input-wrapper input-group form-group"
+                        :class="{ error: errorClasses.pass }">
+                        <div class="input-group-addon fa fa-lock"></div>
+                        <input class="form-control" type="password" v-model="form.pass" placeholder="New password" />
+                    </div>
+
+                    <div class="input-wrapper input-group form-group"
+                        :class="{ error: errorClasses.repeatPass }">
+                        <div class="input-group-addon fa fa-lock"></div>
+                        <input class="form-control" type="password" v-model="form.repeatPass" placeholder="Repeat new password" />
+                    </div>
+
+                    <button class="btn btn-primary btn-lg" :disabled="formLoading">Change password</button>
+                </form>
+            </section>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+// 3rd party libs
+import axios from 'axios';
+
+// Components
+import loading from '../../components/loading/loading.vue';
+
+const changePasswordPage = {
+    components: {
+        loading
+    },
     props: {
         userData: {
-            type: 'object'
+            type: Object
         }
     },
     data: function() {
@@ -41,8 +90,6 @@ const ChangePassword = {
             this.loading = false;
         },
         submit: function() {
-            let self = this;
-
             this.formLoading = true;
             this.formError = '';
 
@@ -73,20 +120,20 @@ const ChangePassword = {
                 pass: this.form.pass,
                 repeatPass: this.form.repeatPass
             })
-            .then(function (response) {
-                self.$parent.displayMessage(response.data.message, 'success');
-                self.formLoading = false;
-                self.form = {
+            .then((response) => {
+                this.$parent.displayMessage(response.data.message, 'success');
+                this.formLoading = false;
+                this.form = {
                     oldPass: '',
                     pass: '',
                     repeatPass: ''
                 };
             })
-            .catch(function (error) {
-                self.formLoading = false;
+            .catch((error) => {
+                this.formLoading = false;
 
                 // Display error message from API
-                self.formError = error.response.data.message;
+                this.formError = error.response.data.message;
 
                 let errorFields = error.response.data.fields;
 
@@ -97,22 +144,23 @@ const ChangePassword = {
 
                 // Mark fields with error class
                 for (let i = 0; i < errorFields.length; ++i) {
-                    self.errorClasses[errorFields[i]] = true;
+                    this.errorClasses[errorFields[i]] = true;
                 }
             });
         }
-    },
+    }
 };
 
 // Routing
 pce.routes.push({
     path: '/profile/change-password',
-    component: ChangePassword,
-    props: true,
+    component: changePasswordPage,
     meta: {
         loggedIn: true,
         title: 'Change Password - Profile',
-        template: 'change-password',
         description: 'User page to change password'
     }
 });
+
+export default changePasswordPage;
+</script>
