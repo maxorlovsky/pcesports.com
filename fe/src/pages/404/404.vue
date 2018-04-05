02a94364062a7bff2c8aa185633b65f257515e9d
Max Orlovsky <max.orlovsky@gmail.com>
2018-04-05 16:05:44 +0200

    feat(404): move to vue single file structure

---
+++ fe/src/pages/404/404.vue
@@ -0,0 +1,38 @@
+<template>
+<div class="error-404 block">
+    <div class="maintenance block-content">
+        <h2>Page not found</h2>
+        <img src="/dist/assets/images/maintenance.png" />
+        <router-link :to="'/'">
+            <button class="btn btn-primary btn-lg">Go to home page</button>
+        </router-link>
+    </div>
+</div>
+</template>
+
+<script>
+// 3rd party libs
+import axios from 'axios';
+
+// Global functions
+import { functions } from '../../functions.js';
+
+const pageNotFound = {
+    data: function () {
+        window.location.href = "../404.html";
+        return {};
+    }
+};
+
+// Routing
+pce.routes.push({
+    path: '/404',
+    component: pageNotFound,
+    meta: {
+        title: 'Page not found',
+        description: 'Page not found'
+    }
+});
+
+export default pageNotFound;
+</script>
