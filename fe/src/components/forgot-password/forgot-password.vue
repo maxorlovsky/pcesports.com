457cd13d25cada39156870983a96be07093516c7
Max Orlovsky <max.orlovsky@gmail.com>
2018-04-05 16:21:00 +0200

    feat(forgot-password): move to vue single file structure

--- fe/src/components/forgot-password/forgot-password.vue
+++ fe/src/components/forgot-password/forgot-password.vue
@@ -1,12 +1,59 @@
-Vue.component('forgot-password', {
-    template: dynamicTemplates.forgotPassword,
+<template>
+<div class="forgot-password-form">
+    <div class="alert alert-success" v-if="restoreSuccess">{{restoreSuccess}}</div>
+    <div class="alert alert-danger" v-if="restoreError" v-html="restoreError"></div>
+
+    <form method="post" v-on:submit.prevent="submit()" v-if="showForm === 1">
+        <div class="input-wrapper"
+            :class="{ error: errorClasses.login }">
+            <input type="text" v-model="form.login" placeholder="Email" />
+        </div>
+
+        <div class="input-wrapper">
+            <div class="g-recaptcha" id="fp-g-recaptcha" :data-sitekey="captchaKey"></div>
+        </div>
+
+        <button class="btn btn-primary" :disabled="formLoading">Restore password</button>
+    </form>
+
+    <form method="post" v-on:submit.prevent="submitReset()" v-if="showForm === 2">
+        <div class="input-wrapper"
+            :class="{ error: errorClasses.code }">
+            <input type="text" v-model="resetForm.code" placeholder="Code" />
+        </div>
+
+        <div class="input-wrapper"
+            :class="{ error: errorClasses.pass }">
+            <input type="password" v-model="resetForm.pass" placeholder="New password" />
+        </div>
+
+        <div class="input-wrapper"
+            :class="{ error: errorClasses.repeatPass }">
+            <input type="password" v-model="resetForm.repeatPass" placeholder="Repeat new password" />
+        </div>
+
+        <button class="btn btn-primary" :disabled="formLoading">Set up new password</button>
+    </form>
+
+    <a href="javascript:;" v-on:click="openLoginMenu()">Back to login form</a>
+</div>
+</template>
+
+<script>
+// 3rd party libs
+import axios from 'axios';
+
+// Global functions
+import { functions } from '../../functions.js';
+
+export default {
+    name: 'forgot-password',
     data: function() {
         return {
             restoreError: '',
             restoreSuccess: '',
             captchaId: 0,
             captchaKey: '6LcwJ_8SAAAAAL2SgH-NYduvEp9DLUlndHrlMs7Z',
-            loading: false,
             formLoading: false,
             showForm: 1,
             form: {
@@ -32,8 +79,6 @@ Vue.component('forgot-password', {
     },
     methods: {
         submit: function() {
-            let self = this;
-
             this.formLoading = true;
             this.restoreSuccess = '';
             this.restoreError = '';
@@ -50,22 +95,20 @@ Vue.component('forgot-password', {
                 login: this.form.login,
                 captcha: grecaptcha.getResponse(this.captchaId)
             })
-            .then(function (response) {
-                self.restoreSuccess = response.data.message;
-                self.showForm = 2;
-                self.formLoading = false;
-                self.checkCaptcha();
+            .then((response) => {
+                this.restoreSuccess = response.data.message;
+                this.showForm = 2;
+                this.formLoading = false;
+                this.checkCaptcha();
             })
-            .catch(function (error) {
-                self.restoreError = error.response.data.message;
-                self.formLoading = false;
-                self.errorClasses.login = true;
-                self.checkCaptcha();
+            .catch((error) => {
+                this.restoreError = error.response.data.message;
+                this.formLoading = false;
+                this.errorClasses.login = true;
+                this.checkCaptcha();
             });
         },
         submitReset: function() {
-            let self = this;
-
             this.formLoading = true;
             this.restoreSuccess = '';
             this.restoreError = '';
@@ -94,16 +137,16 @@ Vue.component('forgot-password', {
                 pass: this.resetForm.pass,
                 repeatPass: this.resetForm.repeatPass
             })
-            .then(function (response) {
-                pce.storage('set', 'token', { "sessionToken": response.data.sessionToken }, 604800000); // 7 days
-                self.$parent.$emit('right-menu');
-                self.$parent.$parent.login();
-                self.$parent.$parent.displayMessage(response.data.message, 'success');
-                self.$router.push('/profile');
+            .then((response) => {
+                functions.storage('set', 'token', { "sessionToken": response.data.sessionToken }, 604800000); // 7 days
+                this.$parent.$emit('right-menu');
+                this.$parent.$parent.login();
+                this.$parent.$parent.displayMessage(response.data.message, 'success');
+                this.$router.push('/profile');
             })
-            .catch(function (error) {
-                self.restoreError = error.response.data.message;
-                self.formLoading = false;
+            .catch((error) => {
+                this.restoreError = error.response.data.message;
+                this.formLoading = false;
 
                 let errorFields = error.response.data.fields;
 
@@ -114,7 +157,7 @@ Vue.component('forgot-password', {
 
                 // Mark fields with error class
                 for (let i = 0; i < errorFields.length; ++i) {
-                    self.errorClasses[errorFields[i]] = true;
+                    this.errorClasses[errorFields[i]] = true;
                 }
             });
         },
@@ -130,4 +173,5 @@ Vue.component('forgot-password', {
             }
         }
     }
-});
+}
+</script>
