90119fd42192ed21895ad4aefcba205e646307a6
Maxtream <max.orlovsky@gmail.com>
2020-01-25 17:06:29 +0100

    feat(v4.0): update config files
    - remove redundant files
    - move folder from /fe/ into source folder as there is no need for it

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,25 @@
 const path = require('path');
-const webpack = require('webpack');
-
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const fs = require('fs');
+const packageFile = fs.readFileSync('package.json');
+const version = JSON.parse(packageFile).version;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const ReplaceInFileWebpackPlugin = require('replace-in-file-webpack-plugin');
 const globImporter = require('node-sass-glob-importer');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const config = {
     mode: 'development',
     entry: {
-        bundle: ['./fe/src/main.js'],
-        combined: ['./fe/styles/global.scss']
+        bundle: ['@babel/polyfill', './src/main.js'],
+        styles: ['./styles/global.scss']
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname, './public/dist')
+        path: path.resolve('./public/dist')
     },
     module: {
         rules: [
@@ -22,27 +29,26 @@ const config = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env']
+                        presets: ['@babel/env']
                     }
                 }
             },
             {
                 test: /\.vue$/,
-                loader: 'vue-loader',
-                options: {
-                    loaders: {
-
-                    }
-                }
+                loader: 'vue-loader'
             },
             {
                 test: /\.scss$/,
-                use: ExtractTextPlugin.extract([
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
                     {
-                        loader: 'raw-loader'
-                    },
-                    {
-                        loader: 'postcss-loader'
+                        loader: 'postcss-loader',
+                        options: {
+                            config: {
+                                path: './postcss.config.js'
+                            }
+                        }
                     },
                     {
                         loader: 'sass-loader',
@@ -50,47 +56,136 @@ const config = {
                             importer: globImporter()
                         }
                     }
-                ])
+                ]
             }
         ]
     },
     resolve: {
         extensions: ['.vue', '.js', '.html'],
-        modules: [
-            'node_modules'
-        ],
+        modules: ['node_modules'],
         alias: {
             vue: 'vue/dist/vue.js'
         }
     },
     plugins: [
-        new ExtractTextPlugin({
+        new MiniCssExtractPlugin({
             filename: '[name].css'
-        })
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            filename: '../index.html',
+            alwaysWriteToDisk: true,
+            inject: false
+        }),
+        new HtmlWebpackHarddiskPlugin(),
+        new VueLoaderPlugin()
     ],
     optimization: {
         splitChunks: {
             cacheGroups: {
                 commons: {
                     test: /[\\/]node_modules[\\/]/,
-                    name: "vendors",
-                    chunks: "all"
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        },
+        minimizer: []
+    },
+    devtool: 'source-map',
+    devServer: {
+        compress: true,
+        https: false,
+        port: 8300,
+        historyApiFallback: true,
+        contentBase: './public/',
+        publicPath: '/dist/',
+        watchOptions: {
+            ignored: /node_modules/
+        },
+        inline: true,
+        quiet: false,
+        public: 'pcesports-fe-web.maxorlovsky.net',
+        proxy: {
+            '/api': {
+                target: 'http://localhost:8325/public',
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            },
+            '/wp-api': {
+                target: 'http://localhost:8325/public',
+                secure: false,
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/wp/wp-json/': ''
                 }
             }
         }
     },
-    devtool: "source-map"
+    performance: {
+        hints: false
+    }
 };
 
-module.exports = function (env = {}, argv) {
+module.exports = (env = {}) => {
+    if (env.production) {
+        config.mode = 'production';
+        config.devtool = false;
+
+        config.resolve.alias.vue = 'vue/dist/vue.min';
+
+        config.optimization.minimizer.push(new UglifyJSPlugin({
+            cache: true,
+            parallel: true,
+            sourceMap: false
+        }));
+    }
+
     if (env.dashboard) {
         const Dashboard = require('webpack-dashboard');
         const DashboardPlugin = require('webpack-dashboard/plugin');
         const dashboard = new Dashboard({ port: 9000 });
-        config.plugins.push(
-            new DashboardPlugin(dashboard.setData)
-        );
+
+        config.plugins.push(new DashboardPlugin(dashboard.setData));
     }
+
+    const copyFiles = [
+        {
+            from: './assets/',
+            to: 'assets/'
+        },
+        {
+            from: './node_modules/font-awesome/fonts/',
+            to: 'assets/fonts'
+        }
+    ];
+
+    config.plugins.push(new CopyWebpackPlugin(copyFiles));
+
+    const replaceInFileRules = [
+        {
+            search: /%version%/g,
+            replace: version
+        }
+    ];
+
+    if (env.production) {
+        replaceInFileRules.push({
+            search: /(<!-- dev -->)([\s\S]*?)(<!-- !dev -->)/g,
+            replace: ''
+        });
+    }
+
+    config.plugins.push(new ReplaceInFileWebpackPlugin([
+        {
+            dir: path.resolve('./public/'),
+            files: ['index.html'],
+            rules: replaceInFileRules
+        }
+    ]));
 
     return config;
 };
