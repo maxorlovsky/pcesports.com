f9e3b1c6701354f0a46f9d53a3de3049ff63cd3b
Max Orlovsky <max.orlovsky@gmail.com>
2024-09-08 15:17:26 +0000

    fix(not-found): add correct way how to handle 404 pages for prerender

--- frontend/src/pages/not-found/index.tsx
+++ frontend/src/pages/not-found/index.tsx
@@ -1,10 +1,21 @@
-const metaCode = document.createElement('meta');
-
-metaCode.name = 'prerender-status-code';
-metaCode.setAttribute('content', '404');
-document.getElementsByTagName('head')[0].appendChild(metaCode);
+// 3rd party libs
+import { useEffect } from 'react';
 
 const NotFound = () => {
+  useEffect(() => {
+    const metaCode = document.createElement('meta');
+
+    metaCode.name = 'prerender-status-code';
+    metaCode.setAttribute('content', '404');
+    document.getElementsByTagName('head')[0].appendChild(metaCode);
+
+    // Returning a cleanup function
+    return () => {
+      // Remove metaCode
+      document.querySelector('meta[name="prerender-status-code"]')?.remove();
+    };
+  }, []);
+
   return (
     <div className='not-found'>
       <h1>Page not found</h1>
